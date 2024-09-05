c17196debc1c9a6eda7602841a8ab5bd346c7f90
nina-1074862 <1074862@hr.nl>
2024-09-05 15:17:55 +0200

    Finished Lesson 1

--- Opdrachten/Uitwerkingen/Opdracht les 1 uitwerking.py
+++ Opdrachten/Uitwerkingen/Opdracht les 1 uitwerking.py
@@ -1,30 +1,112 @@
-# Opdracht Les 1 Uitwerking (Nina)
+# Opdracht Les 1 Uitwerking (nina-1074862) FYI I haven't worked with python in months so pls don't laugh lmao
 
-# I haven't used python in months so don't laugh please
-import math
+# Standaard rekenoperatoren
+# Maak de volgende berekeningen en denk bij elke berekening na over het te verwachten resultaat. Voer de berekeningen uit in de REPL of in je IDE en kijk of het resultaat overeenkomt met je verwachting:
 
-# Print x for answer
+# Print x for output
 x = (2 + 3)
 print(x)
 
-2 - 3
+x = (2 - 3)
+print(x)
 
-2 * 3
+x = (2 * 3)
+print(x)
 
-2 / 3
+x = (2 / 3)
+print(x)
 
-2 // 3
+x = (2 // 3)
+print(x)
 
-2 ** 3
+x = (2 ** 3)
+print(x)
 
-3 + 2
+x = (3 + 2)
+print(x)
 
-3 - 2
+x = (3 - 2)
+print(x)
 
-3 * 2
+x = (3 * 2)
+print(x)
 
-3 / 2
+x = (3 / 2)
+print(x)
 
-3 // 2
+x = (3 // 2)
+print(x)
 
-3 ** 2
+x = (3 ** 2)
+print(x)
+
+# Haakjes
+# Print x for output
+x = (23.95 * 32.9)
+print(x)
+
+x = (23.95 * 32.9) + 6
+print(x)
+
+x = (23.95 * 32.9) + 6
+print(x)
+
+x = 23.95 * (32.9 + 6)
+print(x)
+
+x = (23.95 / 32.9)
+print(x)
+
+x = (23.95 / 32.9 + 6)
+print(x)
+
+x = (23.95 / 32.9) + 6
+print(x)
+
+x = (23.95 / (32.9 + 6))
+print(x)
+
+# Variabelen
+name = "John Doe"
+print(name)
+
+street = 'Neverland Lane'
+print(street)
+
+items = 5
+print(items)
+
+price = 19.95
+print(price)
+
+students = 28
+teachers = 3
+seats = students + teachers
+print(seats)
+
+correct = students > teachers
+print(correct)
+
+incorrect = students > seats
+print(incorrect)
+
+line_character = '-'
+line = line_character * 20
+print(line)
+
+# Combineren
+# Je kunt variabelen combineren met tekst. Dit heet een formatted string. Je kunt dit doen met de f-string notatie. Hierbij zet je een f voor de string en kun je variabelen tussen accolades zetten. Python vervangt de variabelen dan met de waarde van de variabele.
+city = 'Rotterdam'
+print(city)
+type(city)
+
+residents = 623652
+print(residents)
+type(residents)
+
+area = 324.1
+print(area)
+type(area)
+
+# Formatted string, variabele in string weergeven
+print(f'The city of {city} has {residents} on an area of {area} square kilometers')
