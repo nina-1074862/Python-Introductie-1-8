1917c1c9f0cb03afe999e9378b74c26af2536294
nina-1074862 <1074862@hr.nl>
2024-09-08 17:28:25 +0200

    Update Opdracht les 4 uitwerking.py

---
+++ Opdrachten/Uitwerkingen/Opdracht les 4 uitwerking.py
@@ -0,0 +1,5 @@
+# Opdracht les 4 uitwerking
+
+# List
+
+# Een lijst is een verzameling van elementen. Deze elementen kunnen van elk data type zijn. Een lijst wordt gedefinieerd door vierkante haken [] te gebruiken. De elementen in de lijst worden gescheiden door een komma ,.
