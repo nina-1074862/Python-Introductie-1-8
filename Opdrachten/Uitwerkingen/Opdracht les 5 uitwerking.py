3ac2e7e339a74e99f4606ad7c93db362fb70bba3
nina-1074862 <1074862@hr.nl>
2024-09-10 00:03:48 +0200

    Update Opdracht les 5 uitwerking.py

--- Opdrachten/Uitwerkingen/Opdracht les 5 uitwerking.py
+++ Opdrachten/Uitwerkingen/Opdracht les 5 uitwerking.py
@@ -39,4 +39,63 @@ for number in numbers:
         exit()
 
 # Als je het jezelf moeilijker wilt maken kun je proberen ditzelfde programma op te lossen niet met een "for .. in" maar met een "while" loop en één van de vele functies die je kunt aanroepen op een lijst object. Je zou hier ook de "walrus" operator kunnen toepassen, een recente toevoeging aan Python.
-# Breaks 
+# Breaks en continues
+# Een andere techniek die je veel zult gebruiken is het onderbreken van een loop. De twee meest voorkomende manieren zijn "break" en "continue". Met een "break" stop je de loop helemaal. Met een "continue" ga je naar de volgende iteratie van de loop en voer je opvolgende code in de loop niet uit.
+for i in range(1, 10):
+    if i == 5:
+        continue
+    print(i)
+    if i == 7:
+        break
+
+# Breaks gebruik je bijvoorbeeld als je lijsten of dictionaries moet doorzoeken. Als je betreffende waarde al hebt gevonden (of zeker weet dat die er niet in zit) gebruik je een break om de loop te stoppen en versneld door te gaan met de rest van je programma.
+# Je gaat een programma schrijven dat alle priemgetallen onder de 100 uitrekent. Ter herinnering, een priemgetal is een cijfer dat alleen deelbaar is door zichzelf, door alle andere getallen komt er een niet geheel getal uit. Lus door alle mogelijke delers en gebruik een "break" om te stoppen met zoeken als je een deler hebt gevonden. Print het priemgetal iedere keer dat je zeker weet dat je er één hebt.
+# 1, 2, 3, 5 en 7 zijn bijvoorbeeld priemgetallen.
+# Begin je programma met de volgende code:
+numbers = list(range(1, 100))
+for priemgetal in numbers:
+    if priemgetal == 1:
+        continue
+    for deler in range(2, priemgetal):
+        if priemgetal % deler == 0:
+            break
+    else:
+        print(priemgetal)
+
+# Geneste lijsten
+# Wat je ook regelmatig zult tegenkomen is een geneste lijst, oftewel een lijst met lijsten. Bijvoorbeeld:
+mylist = [ ["appel", "peer"] , ["komkommer", "paprika"]]
+# In dit geval geeft "mylist[0][1]" dus het eerste element van de buitenste lijst (["appel", "peer"]), gevolgd door daar dan weer het tweede element uit ("peer").
+
+# Opdracht 1:
+# In het klassieke bingo spel krijg je een kaart met willekeurige getallen van 1 tot 99, bijvoorbeeld 16 getallen in een 4x5 rooster. Vervolgens worden er een aantal getallen omgeroepen en heb je één rij of kolom helemaal vol, dan heb je "bingo" en win je.
+# Schrijf een programma dat een bingokaart genereert, in 4x4 formaat (4 rijen, 4 kolommen). Gebruik als uitgangspunt de volgende code om een lijst met willekeurige cijfers te genereren:
+# We hebben de random module nodig om willekeurige getallen te genereren
+
+import random
+# Totaal aantal getallen op de kaart zal hoogte x breedte zijn
+bingo_number_total = 4 ** 2
+# Daarna maken we een lijst met 99 getallen waar we uit gaan kiezen
+numbers_all = list(range(1, 100))
+# We gooien alle ballen door elkaar
+random.shuffle(numbers_all)
+# ..en pakken de eerste 16 getallen
+bingo_numbers = numbers_all[:bingo_number_total]
+bingo_numbers_card = [
+    bingo_numbers[:4],
+    bingo_numbers[4:8],
+    bingo_numbers[8:12],
+    bingo_numbers[12:]
+]
+for row in bingo_numbers_card:
+    print([', ' .join(map(str, row))])
+
+# Een voorbeeld output zou er als volgt uit kunnen zien:
+#       [22, 35, 86, 45]
+#       [11, 20, 31, 40]
+#       [7, 91, 67, 0]
+#       [10, 72, 27, 55]
+
+# Opdracht 2: Trek 50 willekeurige cijfers en streep die uit je bingo kaart weg.  Bijvoorbeeld door er het getal 0 in te vullen. Druk het resultaat af.
+
+
