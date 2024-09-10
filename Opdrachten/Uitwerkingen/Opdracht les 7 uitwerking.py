19326caea44c8953c7a4b856258d8a22f805848f
nina-1074862 <1074862@hr.nl>
2024-09-10 12:42:34 +0200

    Update Opdracht les 7 uitwerking.py

--- Opdrachten/Uitwerkingen/Opdracht les 7 uitwerking.py
+++ Opdrachten/Uitwerkingen/Opdracht les 7 uitwerking.py
@@ -1,41 +1,5 @@
 # Opdracht Les 7 Uitwerking (nina-1074862)
 
-# Functies
-# Functies zijn een manier om code te groeperen en te hergebruiken. Je kan een functie zien als een klein programmaatje binnen je programma. Je kan een functie aanroepen, waardes mee geven om mee te werken en deze kan dan een waarde teruggeven.
-
-# Code splitsen in functies heeft een heleboel voordelen:
-
-# Je voorkomt dat je code gaat dupliceren. Gedupliceerde code is lastig leesbaar en onderhoudbaar.
-# Je kunt code hergebruiken, zowel in hetzelfde programma als in andere programma's.
-# Je kunt code makkelijker testen en debuggen.
-# Het is makkelijker om samen te werken in code die gesplitst is in functies
-# Code die je schrijft moet je zo snel mogelijk in functies zetten. Je zult zien dat je code veel leesbaarder wordt en dat je veel minder fouten maakt.
-
-# Functie aanroepen
-# Een functie aanroepen doe je door de naam van de functie te gebruiken met daarachter haakjes. Bijvoorbeeld:
-
-def print_hello():
-    print('Hello')
-print_hello()
-
-# Functies kun je argumenten meegeven. De variabelen die je mee geeft in de functie aanroep worden op volgorde aangenomen als argumenten in de functies. Deze argumenten kun je gebruiken binnen de functie. Bijvoorbeeld:
-def print_hello(name):
-    print(f'Hello {name}')
-
-print_hello('John')
-
-# of, een voorbeeld met meerdere argumenten:
-def greeter(name, greeting_type):
-    if greeting_type == 'formal':
-        greeting = 'Hello'
-    elif greeting_type == 'informal':
-        greeting = 'Hi'
-    else:
-        greeting = 'Hey'
-    print(f'{greeting} {name}')
-
-greeter('John', 'formal')
-
 # Opdracht: Rekenmachine
     # We gaan een simpele rekenmachine maken die de volgende functies heeft:
 
@@ -49,17 +13,9 @@ greeter('John', 'formal')
     # subtract(1, 2) drukt -1 af
     # multiply(1, 2) drukt 2 af
     # divide(1, 2) drukt 0.5 af
-# ..daarnaast moet de rekenmachine ook nog een functie hebben die de gebruiker vraagt om twee getallen en een operator, en vervolgens de juiste functie aanroept. Bijvoorbeeld:
-
-input1 = input('Geef het eerste getal: ')
-input2 = input('Geef het tweede getal: ')
-operator = input('Geef de operator (+, -, * of /): ')
+# daarnaast moet de rekenmachine ook nog een functie hebben die de gebruiker vraagt om twee getallen en een operator, en vervolgens de juiste functie aanroept. Bijvoorbeeld:
 
 # Let wel, hier gebeurt nog iets bijzonders. De functie input geeft een string terug en daar kunnen we niet mee rekenen. Voer bijvoorbeeld de volgende code uit:
-string1 = "1"
-string2 = "2"
-print(string1 + string2)  # drukt "12" af
-print(string1 - string2)  # geeft een error (strings kun je niet van elkaar aftrekken)
 
 # We moeten dus de string omzetten naar een getal. Dat kan met de functie int of float. Bijvoorbeeld:
 input1 = input('Geef het eerste getal: ')
@@ -120,11 +76,10 @@ print_hello('Jane')  # drukt "Hello Jane" af
 
 # Opdracht: Rekenmachine met extra output
 # Pas de functies van de rekenmachine aan om een derde waarde te accepteren, "debug", met een standaard waarde van False. Als de waarde True is, druk dan naast het antwoord de hele som af én een waarschuwing als één van de getallen een 0 is. Bijvoorbeeld:
-add(1, 2, True) # drukt "Debug: 1 + 2" en daarna "3" af
-add(1, 2) # drukt "3" af
-# Voeg een vraag toe voor de gebruiker om te vragen of hij debug informatie wil zien.
+# Voeg een vraag toe voor de gebruiker om te vragen of hij debug-informatie wil zien.
 
 # Mijn uitwerking Opdracht: Rekenmachine met extra output
+
 def add(x,y, debug=False):
     if debug:
         print(f"Debug: {x} + {y}")
@@ -158,7 +113,7 @@ while True:
     operator = input('Geef de operator (+, -, * of /): ')
     debug_ask = input("Do you want to see the debug information? [Yes/No] ")
     debug = False
-    if debug_ask.lower() == 'YES':
+    if debug_ask.lower() == 'yes':
         debug = True
 
     # check if choice is one of the four options
@@ -211,81 +166,122 @@ def print_person(name, age, city, country):
     print(f'{name} is {age} jaar oud en woont in {city}, {country}')
 
 print_person(name='John', age=42, city='Rotterdam', country='Nederland')
-
-# ..als we nu een extra argument toevoegen, dan hoeven we de aanroep niet aan te passen.
+# als we nu een extra argument toevoegen, dan hoeven we de aanroep niet aan te passen.
 
 # Opdracht: Refactoring
 # "Refactoring" is een kreet die je nog veel zult tegenkomen. Refactoring slaat op het herschrijven van code, zonder dat de input of output verandert. Dat doe je vaak omdat je de code beter leesbaar wilt maken, of omdat je de code wilt hergebruiken.
-
 # We gaan de code van een vorige opdracht refactoren. Als je onze uitwerking bekijkt van Les3_Source5.py zijn daar een paar zaken niet heel netjes:
-
 # We vragen gebruikers steeds te kiezen uit een aantal zaken ("KOUD of WARM"), maar die keuze dwingen we niet af. Je kunt nog steeds "LAUW" invoeren waar "KOUD" of "WARM" verwacht wordt. We willen die keuze afdwingen, dat kan bijvoorbeeld met een while loop:
-answer = ""
-while answer != "KOUD" and answer != "WARM":
-    answer = input("Wil je warme of koude drank? (KOUD/WARM) ")
-# Je ziet een aantal regels code rondom vragen steeds terugkomen. Maak een aparte functie die vragen kan stellen en het antwoord terug geeft als return, met controle op de antwoorden zoals hierboven beschreven. Bijvoorbeeld:
-def ask_question(question, valid_answers):
-    ...
-    return answer
-# We hebben keuzes die leiden tot andere keuzes, waardoor we een hele lange lap code krijgen. We willen overzicht krijgen door de code op te splitsen in functies voor in het restaurant eten / meenemen, voor de burgers keuze, voor warme drankjes en een functie voor koude drankjes. De uiteindelijke code zou er dan ongeveer zo uit kunnen zien:
-    food_answer = ask_question("Burgers of drankjes?", ["Burgers", "Drankjes"])
+
+# Mijn uitwerking Opdracht: Refactoring
+# Ordering at Mac Donald's
+eat_in = False
+aborted = False
+
+
+SEPERATOR_CHARACTER = "/"
+def get_valid_answer_text(valid_answers):
+    valid_answers_text = ""
+    if len(valid_answers) > 0:
+        # Deze regel kan simpeler, maar dit is duidelijker
+        # We kiezen er voor om de opties niet meteen in de "question" string mee te geven,
+        # maar de lijst met mogelijke antwoorden mee af te drukken als we de vraag stellen
+        valid_answers_text = " ["
+        for answer in valid_answers:
+            valid_answers_text = valid_answers_text + answer + SEPERATOR_CHARACTER
+        valid_answers_text = valid_answers_text[:-1]  # Deze regel verwijdert de laatste /
+        valid_answers_text = valid_answers_text + "]"
+    return valid_answers_text
+
+
+def ask_question(question, valid_answers=()):
+    valid_answers_upper = []
+    # Hier maken we alvast een lijst met de geldige antwoorden in hoofdletters
+    for answer in valid_answers:
+        valid_answers_upper.append(answer.upper())
+
+    # We bouwen de vraag op uit de vraagtekst en de geldige antwoorden (als die er zijn)
+    question_string = question
+    question_string = question_string + get_valid_answer_text(valid_answers)
+    question_string = question_string + ": "
+
+    # We blijven de vraag stellen tot we een geldig antwoord hebben
+    answer_str = ""
+    while answer_str not in valid_answers_upper:
+        answer_str = input(question_string)
+        answer_str = answer_str.upper()
+
+    # We geven het antwoord terug in hoofdletters
+    return answer_str
+
+
+def eat_in_choice():
+    eat_in = False
+    question_1 = ask_question("Hier opeten of meenemen", ["Opeten", "Meenemen"])
+    if question_1 == "OPETEN":
+        # Eat in part
+        print("Hier opeten")
+        eat_in = True
+    elif question_1 == "MEENEMEN":
+        # Take away part
+        print("Meenemen")
+        eat_in = False
+    return eat_in
+
+
+def get_burger_choice():
+    question_3 = ask_question(
+        "Burgers", ["Hamburger", "Cheeseburger", "Big Mac", "Quarter Pounder"]
+    )
+    if question_3 == "HAMBURGER":
+        print("Hamburger")
+    elif question_3 == "CHEESEBURGER":
+        print("Cheeseburger")
+    elif question_3 == "Big MAC":
+        print("Big Mac")
+    elif question_3 == "QUARTER POUNDER":
+        print("Quarter Pounder")
+
+
+def get_warm_drink_choice():
+    question_5 = ask_question("Warme drank", ["Koffie", "Cappucino", "Chocolademelk"])
+    if question_5 == "KOFFIE":
+        print("Koffie")
+    elif question_5 == "CAPPUCINO":
+        print("Cappucino")
+    elif question_5 == "CHOCOLADEMELK":
+        print("Chocolademelk")
+
+
+def get_cold_drink_choice():
+    question_6 = ask_question(
+        "Koude drank ", ["Coca Cola", "Cola Zero", "7-Up", "Fanta", "Fristi"]
+    )
+    if question_6 == "COCA COLA":
+        print("Coca Cola")
+    elif question_6 == "COLA ZERO":
+        print("Cola Zero")
+    elif question_6 == "7-UP":
+        print("7-Up")
+    elif question_6 == "FANTA":
+        print("Fanta")
+    elif question_6 == "FRISTI":
+        print("Fristi")
+
+
+print("Welkom bij de Mac Donald's")
+eat_in = eat_in_choice()
+if eat_in:
+    food_answer = ask_question("Burgers of drankjes", ["Burgers", "Drankjes"])
     if food_answer == "BURGERS":
         get_burger_choice()
     elif food_answer == "DRANKJES":
         drink_choice = ask_question("Drankjes", ["Warme", "Koude"])
-# Neem de oude code en refactor deze, waarbij je bovenstaande drie verbeteringen doorvoert.
-
-# Opdracht: Studenten rapport
-# In deze opdracht gaan we lijsten, dictionaries en functies combineren. We gaan een programma maken dat een rapport afdrukt van een aantal studenten. Van een student heb je de volgende informatie:
-
-# Iedere student zit in één klas, bijvoorbeeld "SWDVT-2023-1A". Niet alle klassen zijn even groot.
-# Iedere student heeft 4 werkplaats vakken (WP1, WP2, WP3 en WP4)
-# Voor ieder vak is het resultaat bekend. De student heeft een "onvoldoende", een "voldoende", een "goed" of een "uitstekend" gescored.
-# We noemen een student "excellent" als hij meer dan één uitstekend heeft of als alle resultaten beter dan "voldoende" zijn.
-# We gaan een rapport maken om aan het einde van het jaar aan de onderwijscoordinator te sturen, met de volgende informatie:
-
-# We noemen een student "excellent" als hij meer dan één uitstekend heeft of als alle resultaten beter dan "voldoende" zijn. Welke studenten zijn excellent?
-# Welke klas(sen) hebben het hoogste percentage "excellent" studenten?
-# Welke klas heeft gemiddeld genomen over alle vakken de hoogste scores? Dit is een lastige. Stel voor het gemak dat een onvoldoende = 0 punten, voldoende 2 punten, goed 3 punten en uitstekend 4 punten.
-# Gegeven diezelfde manier van scoren: studenten met een score van 3 of minder punten moeten een inhaal opdracht doen. Welke studenten zijn dat en wat zijn hun resultaten?
-# Maak gebruik van de volgende functies voor de vier onderzoeksvragen en bedenk dat je jouw dataset waarschijnlijk eerst in verschillende varianten zult moeten omzetten. Bedenk ook dat als je code aan het herhalen bent, dat je dan waarschijnlijk een functie kunt maken.
-def get_excellent_students(list_of_students):
-    ...
-
-def get_most_excellent_classroom(list_of_classrooms):
-    ...
-
-def get_best_scoring_classroom(list_of_classrooms):
-    ...
-
-def get_failed_students(list_of_students, minimum_score = 4):
-    ...
-
-# Je kunt optioneel een dataset genereren met de code in generate_fake_students.py. Nadat je succesvol een bestand hebt weggeschreven importeer je het resultaat met het volgende commando boven in je Python bestand:
-from students_classrooms import students_per_classroom
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if drink_choice == "WARME":
+            get_warm_drink_choice()
+        elif drink_choice == "KOUDE":
+            get_cold_drink_choice()
+if eat_in:
+    print("Bedankt voor uw bestelling en eet smakelijk in ons restaurant.")
+else:
+    print("Bedankt voor uw bestelling, goede reis en eet smakelijk.")
